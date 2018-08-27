1e6347d768048175dc419d4638bc7b1947069781
yoooooona <chldbsk8911@naver.com>
2018-08-27 18:30:50 +0900

    yuna

--- teamProject-client/src/view/LoginForm.java
+++ teamProject-client/src/view/LoginForm.java
@@ -5,18 +5,48 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.Font;
 
-public class LoginForm extends JFrame {
+public class LoginForm extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtid;
+	private JPasswordField txtpassword;
+	private JButton loginbtn, signbtn;
+
+	private Connection con = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://35.190.228.94:3306/teamdb?useSSL=false";
+			con = DriverManager.getConnection(url, "root", "12345");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 
 	/**
 	 * Launch the application.
@@ -38,44 +68,93 @@ public class LoginForm extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginForm() {
+		setTitle("ZZITALK");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 574, 464);
 		contentPane = new JPanel();
+		contentPane.setToolTipText("");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(203, 221, 116, 21);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(203, 267, 116, 21);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setIcon(new ImageIcon("img/login.png"));
-		btnNewButton.setBorder(null);
-		btnNewButton.setBounds(343, 221, 144, 67);
-		panel.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("\uC544\uC774\uB514");
-		lblNewLabel.setBounds(117, 224, 57, 15);
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("\uBE44\uBC00\uBC88\uD638");
-		lblNewLabel_1.setBounds(117, 270, 57, 15);
-		panel.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("LOGO");
-		lblNewLabel_2.setBounds(262, 118, 57, 15);
-		panel.add(lblNewLabel_2);
+
+		txtid = new JTextField();
+		txtid.setBounds(203, 221, 116, 21);
+		panel.add(txtid);
+		txtid.setColumns(10);
+
+		txtpassword = new JPasswordField();
+		txtpassword.setBounds(203, 267, 116, 21);
+		panel.add(txtpassword);
+		txtpassword.setColumns(10);
+
+		JLabel logolabel = new JLabel("LOGO");
+		logolabel.setIcon(new ImageIcon("C:\\Users\\admin\\Desktop\\이미지\\zzitalk.png"));
+		logolabel.setBounds(167, 102, 208, 67);
+		panel.add(logolabel);
+
+		JLabel pwlabel = new JLabel("New label");
+		pwlabel.setIcon(new ImageIcon("C:\\Users\\admin\\Desktop\\이미지\\pw.png"));
+		pwlabel.setBounds(134, 270, 82, 18);
+		panel.add(pwlabel);
+
+		JLabel idlabel = new JLabel("New label");
+		idlabel.setIcon(new ImageIcon("C:\\Users\\admin\\Desktop\\이미지\\ddi.png"));
+		idlabel.setBounds(149, 224, 57, 15);
+		panel.add(idlabel);
+
+		signbtn = new JButton("s i g n u p");
+		signbtn.setForeground(Color.BLACK);
+		signbtn.setBackground(Color.WHITE);
+		signbtn.setFont(new Font("휴먼고딕", Font.BOLD, 12));
+		signbtn.setBounds(213, 298, 116, 31);
+		panel.add(signbtn);
+
+		loginbtn = new JButton("New button");
+		loginbtn.setIcon(new ImageIcon("C:\\Users\\admin\\Desktop\\이미지\\login.png"));
+		loginbtn.setBounds(350, 239, 142, 31);
+		panel.add(loginbtn);
+
+		signbtn.addActionListener(this);
+		loginbtn.addActionListener(this);
 	}
+
+	@Override // 로그인 가능 여부
+	public void actionPerformed(ActionEvent e) {
+
+		String id = txtid.getText(); // id 와 password text 얻어오기
+		String password = txtpassword.getText();
+		String sql = "";
+
+		if (e.getSource().equals(loginbtn)) {
+			con = getConnection();
+			sql = "select * from usertbl where id='"+ id +"' and password = '" + password +"'";
+			try {pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				if (rs.next() == true) {
+					JOptionPane.showMessageDialog(this, "로그인되었습니다.", "login", JOptionPane.OK_OPTION);
+				} else {
+					JOptionPane.showMessageDialog(this, "아이디 또는 비밀번호가 일치하지 않습니다.", "login", JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		else {
+			signupDialog sign = new signupDialog();
+			sign.setVisible(true);
+		}
+	}
+
+	// password 와 confirmpassword가 다르다면
+	/*
+	 * if(txtpassword.getText().isEmpty() == txtconfirm.getText().isEmpty()) {
+	 * JOptionPane.showInputDialog("비밀번호가 일치하지않습니다.",JOptionPane.ERROR_MESSAGE); } }
+	 */
+
 }
---
+++ teamProject-client/src/view/signupDialog.java
@@ -0,0 +1,239 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
+
+public class signupDialog extends JDialog implements ActionListener {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtid, txtnick;
+	private JPasswordField txtpassword, txtconfirm;
+	private JLabel nicklabel;
+	private JButton okbtn, cancelbtn, confirmbtn_id, confirmbtn_nick;
+
+	private Connection con = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	private boolean bool_id = false;
+	private boolean bool_nick = false;
+	
+	
+
+	private Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://35.190.228.94:3306/teamdb?useSSL=false";
+			con = DriverManager.getConnection(url, "root", "12345");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			signupDialog dialog = new signupDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public signupDialog() {
+		setTitle("sign up");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		contentPanel.setBackground(Color.WHITE);
+		contentPanel.setBounds(0, 0, 434, 205);
+		contentPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		getContentPane().add(contentPanel);
+		contentPanel.setLayout(null);
+
+		JLabel idlabel = new JLabel("ID");
+		idlabel.setBounds(55, 27, 57, 15);
+		contentPanel.add(idlabel);
+
+		JLabel passwordlabel = new JLabel("password");
+		passwordlabel.setBounds(33, 60, 57, 15);
+		contentPanel.add(passwordlabel);
+
+		JLabel confirmlabel = new JLabel("Confirm password ");
+		confirmlabel.setBounds(12, 98, 113, 15);
+		contentPanel.add(confirmlabel);
+
+		nicklabel = new JLabel("nickname");
+		nicklabel.setBounds(33, 138, 57, 15);
+		contentPanel.add(nicklabel);
+
+		txtid = new JTextField();
+		txtid.setBounds(139, 24, 116, 21);
+		contentPanel.add(txtid);
+		txtid.setColumns(10);
+
+		confirmbtn_id = new JButton("c o n f i r m");
+		confirmbtn_id.setFont(new Font("휴먼고딕", Font.BOLD, 12));
+		confirmbtn_id.setBackground(Color.WHITE);
+		confirmbtn_id.setBounds(290, 21, 119, 28);
+		contentPanel.add(confirmbtn_id);
+
+		txtpassword = new JPasswordField();
+		txtpassword.setBounds(139, 57, 116, 21);
+		contentPanel.add(txtpassword);
+
+		txtconfirm = new JPasswordField();
+		txtconfirm.setBounds(137, 95, 118, 21);
+		contentPanel.add(txtconfirm);
+
+		txtnick = new JTextField();
+		txtnick.setBounds(139, 135, 116, 21);
+		contentPanel.add(txtnick);
+		txtnick.setColumns(10);
+
+		confirmbtn_nick = new JButton("c o n f i r m");
+		confirmbtn_nick.setFont(new Font("휴먼고딕", Font.BOLD, 12));
+		confirmbtn_nick.setBackground(Color.WHITE);
+		confirmbtn_nick.setBounds(290, 134, 119, 28);
+		contentPanel.add(confirmbtn_nick);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+			buttonPane.setBackground(Color.WHITE);
+			buttonPane.setBounds(0, 205, 434, 57);
+			getContentPane().add(buttonPane);
+			buttonPane.setLayout(null);
+			{
+				cancelbtn = new JButton("Cancel");
+				cancelbtn.setFont(new Font("휴먼고딕", Font.BOLD, 12));
+				cancelbtn.setBackground(Color.WHITE);
+
+				cancelbtn.setBounds(238, 15, 79, 30);
+				cancelbtn.setActionCommand("Cancel");
+				buttonPane.add(cancelbtn);
+
+				okbtn = new JButton("OK");
+				okbtn.setBackground(Color.WHITE);
+				okbtn.setFont(new Font("휴먼고딕", Font.BOLD, 12));
+				okbtn.setBounds(117, 17, 79, 28);
+				buttonPane.add(okbtn);
+
+			}
+		}
+		confirmbtn_id.addActionListener(this);
+		confirmbtn_nick.addActionListener(this);
+		okbtn.addActionListener(this);
+		cancelbtn.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String id = txtid.getText();
+		char[] password = txtpassword.getPassword();
+		char[] confirmpassword = txtconfirm.getPassword();
+		String nickname = txtnick.getText();
+		String sql = "";
+
+		con = getConnection();
+		// 아이디 중복체크 여부
+		if (e.getSource().equals(confirmbtn_id)) {
+			sql = "select * from usertbl where id='" + txtid.getText() + "'";
+			try {
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery(sql);
+
+				if (rs.next() == true) {
+					JOptionPane.showMessageDialog(txtid, "해당 아이디는 현재 사용중입니다. 다시 작성해주세요.", "sign up",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(txtid, "사용가능한 아이디 입니다.", "sign up", JOptionPane.OK_OPTION);
+					bool_id = true;
+				}
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+
+		else if (e.getSource().equals(confirmbtn_nick)) {
+			// 닉네임 중복체크 여부
+			sql = "select * from usertbl where nick=" + txtnick.getText() + "";
+			try {
+				if (rs.next() == true) {
+					JOptionPane.showMessageDialog(txtnick, "해당 닉네임은 현재 사용중입니다. 다시 작성해주세요.", "sign up",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					JOptionPane.showMessageDialog(this, "사용가능한 닉네임입니다.", "sign up", JOptionPane.OK_OPTION);
+					bool_nick = true;
+				}
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+
+		}
+
+		// ok버튼 클릭시
+		// 모든 textfield에 빈칸이 있는지
+		if (e.getSource().equals(okbtn)) {
+			if ((txtid.getText().isEmpty() == true || txtpassword.getText().isEmpty() == true)
+					|| (txtconfirm.getText().isEmpty() == true || txtnick.getText().isEmpty() == true)) {
+				JOptionPane.showMessageDialog(this,"빈칸이 있습니다.","sign up",JOptionPane.ERROR_MESSAGE);
+			}else if(!bool_id){
+				JOptionPane.showMessageDialog(this,"아이디 중복 확인을 해주세요.","sign up",JOptionPane.ERROR_MESSAGE);	
+			}
+			else if(!bool_nick) {
+				JOptionPane.showMessageDialog(this,"닉네임 중복 확인을 해주세요.","sign up",JOptionPane.ERROR_MESSAGE);
+			}
+			
+			else {
+				con = getConnection();
+				sql = "insert into usertbl values ('" + txtid.getText()+ "', '" + txtnick.getText() + "','" + txtpassword.getText() + "')";
+				try {
+					pstmt = con.prepareStatement(sql);
+					int result = pstmt.executeUpdate();
+
+				if (result > 0) {
+					JOptionPane.showMessageDialog(this,"회원가입이 성공적으로 이루어졌습니다.","sign up", JOptionPane.OK_OPTION);
+				} else {
+					JOptionPane.showMessageDialog(this, "sad", "sign up", JOptionPane.OK_OPTION);
+				}
+				
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+
+			}
+
+			}else if(e.getSource()==cancelbtn){
+				this.setVisible(false);
+
+		}
+	}
+}
